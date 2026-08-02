50395c450211e16bc8c0ff2967c3b7c730e7d6cf
Tachibana <a359938435@163.com>
2026-08-02 21:55:48 +0800

    v1

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -8,8 +8,10 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -37,7 +39,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         {
         return Result.fail("店铺不存在");
         }
-        stringRedisTemplate.opsForValue().set("cache:shop:" + id, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set("cache:shop:" + id, JSONUtil.toJsonStr(shop),30, TimeUnit.MINUTES);
+        return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        updateById(shop);
+        Long id = shop.getId();
+        if(id==null)
+        {
+            return Result.fail("店铺id不能为空!");
+        }
+        stringRedisTemplate.delete("cache:shop:" +id);
         return Result.ok(shop);
     }
 }
